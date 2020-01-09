d9f4b6e229d6087e453b394d5706251118e2f1b7
Joao Pedro <joaopedrobeckland@gmail.com>
2020-01-09 16:49:08 -0300

    feat: add a new configuration in Session Controller
    Add a new configuration in Session Controller

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -1,13 +1,17 @@
-"use strict";
+'use strict'
+
+const User = use('App/Models/User')
 
 class SessionController {
-  async store({ request, response, auth }) {
-    const { email, password } = request.only(["email", "password"]);
+  async store ({ request, response, auth }) {
+    const { email, password } = request.only(['email', 'password'])
 
-    const { token } = await auth.attempt(email, password);
+    const { token } = await auth.withRefreshToken().attempt(email, password)
 
-    return { token };
+    const { id, name } = await User.findBy('email', email)
+
+    return { token, user: { id, name, email } }
   }
 }
 
-module.exports = SessionController;
+module.exports = SessionController
